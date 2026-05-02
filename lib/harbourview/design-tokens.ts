02df94d68494ad0aa0ae0b9d1bce2c50bc09ce61
Harbourview <harbourviewcompany@gmail.com>
2026-05-02 17:46:26 -0400

    Add Harbourview design tokens

---
+++ lib/harbourview/design-tokens.ts
@@ -0,0 +1,11 @@
+export const hvTokens = {
+  colors: {
+    black: '#030507',
+    nearBlack: '#050A10',
+    deepNavy: '#07172A',
+    oceanNavy: '#0A2138',
+    gold: '#C6A55A',
+    darkGold: '#A8842D',
+    offWhite: '#F5F1E8',
+  },
+}
