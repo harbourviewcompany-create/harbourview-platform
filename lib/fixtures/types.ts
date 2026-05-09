90aabf1707eb86b7f6d4deca11e53b872003434d
Harbourview <harbourviewcompany@gmail.com>
2026-05-09 14:42:23 -0400

    Allow used surplus reply address projection type

--- lib/fixtures/types.ts
+++ lib/fixtures/types.ts
@@ -36,7 +36,7 @@ export interface NewProductListing extends Listing {
   condition: 'new'
 }
 
-export interface UsedSurplusListing extends Listing {
+export interface UsedSurplusListing extends ListingWithReplyAddress {
   category: 'used-surplus'
   condition: 'used' | 'refurbished' | 'surplus'
   hoursUsed?: string
