aaccbb22fce35ff7a3da59c33f33fe4f17774297
Harbourview <harbourviewcompany@gmail.com>
2026-05-09 15:09:43 -0400

    Narrow genetics storage Supabase payload types

--- lib/introduction-routing/geneticsStorage.ts
+++ lib/introduction-routing/geneticsStorage.ts
@@ -1,6 +1,7 @@
 import { GeneticsRoutingRecord } from './geneticsExecution'
 
 type SupabaseMutationResult = PromiseLike<{ data: unknown; error: unknown }>
+type SupabasePayload = Record<string, unknown>
 
 type SupabaseInsertResult = {
   select?: (cols?: string) => {
@@ -10,8 +11,8 @@ type SupabaseInsertResult = {
 
 type SupabaseLike = {
   from: (table: string) => {
-    insert: (payload: any) => SupabaseInsertResult
-    update: (payload: any) => { eq: (column: string, value: string) => SupabaseMutationResult }
+    insert: (payload: SupabasePayload) => SupabaseInsertResult
+    update: (payload: SupabasePayload) => { eq: (column: string, value: string) => SupabaseMutationResult }
   }
 }
 
