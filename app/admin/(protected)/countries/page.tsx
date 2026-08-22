645af4f0f0850d102227358d106e58dc1037bc28
Harbourview <harbourviewcompany@gmail.com>
2026-08-22 15:42:53 -0400

    feat(admin): route page for Countries panel

---
+++ app/admin/(protected)/countries/page.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable */
+// @ts-nocheck
+'use client'
+
+import { useMemo } from 'react'
+import { mkApi, panelCss, useAdminToast } from '@/components/admin/panels/shared'
+import { Countries } from '@/components/admin/panels/Countries'
+
+export default function Page() {
+  const api = useMemo(() => mkApi(), [])
+  const { toast, toastNode } = useAdminToast()
+  return (
+    <>
+      <style>{panelCss}</style>
+      <div className="hv-app"><div className="hv-main"><div className="content">
+        <Countries api={api} toast={toast} />
+      </div></div></div>
+      {toastNode}
+    </>
+  )
+}
